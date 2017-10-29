798ef8425c83dfa3bdedf97271dae8a11881b2bb
SnowFox <winsource6@hotmail.com>
2017-10-29 11:04:23 +0000

    Project Setup

---
+++ HerbTrans.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerbTrans.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
